321fa473aa3e082970734bc270883a7781900b78
mrzinofc <145413073+mrzinofc@users.noreply.github.com>
2024-09-15 11:44:42 -0300

    DevConverter

---
+++ script.js
@@ -0,0 +1,50 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit)
+
+const inputValue = document.getElementById('value-real');
+const selectedCurrency = document.getElementById('currency');
+const result = document.getElementById('result')
+let valueConverted = 0;
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    if(!inputValue.value || inputValue.value < 0) {
+        alert('Informe um valor correto!');
+        return;
+    } else if(!selectedCurrency.value) {
+        alert('Escolha uma moeda!')
+        return;
+    }
+
+    converter();
+};
+
+function converter() {
+    if(selectedCurrency.value === 'eur') {
+        valueConverted = inputValue.value / 6.17;
+        result.innerHTML = valueFormatter('pt-br', 'EUR');
+
+        animateResult();
+    } else if(selectedCurrency.value === 'dol') {
+        valueConverted = inputValue.value / 5.57;
+        result.innerHTML = valueFormatter('en-US', 'USD');
+
+        animateResult();
+    }
+
+    inputValue.value = '';
+    selectedCurrency.value = '';
+};
+
+function valueFormatter(Locale, currency) {
+    const value = valueConverted.toLocaleString(`${Locale}`, {style: 'currency', currency: `${currency}` })
+    return `<span>💵</span> ${value} <span>💵</span>`;
+};
+
+function animateResult() {
+    return result.animate([
+        { transform: 'translateY(-150px)'},
+        { transform: 'translateY(0px)'},
+    ], { duration: 500 });
+}
